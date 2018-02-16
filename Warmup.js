0cc12b587e7e03ff6f4cfbf7675cd6cceb9963fd
Jaffar Zaidi <jzaidi@fordham.edu>
2018-02-16 10:22:31 -0500

    Second commit

---
+++ Warmup.js
@@ -0,0 +1,9 @@
+var cars = ['Toyota', 'Audi', 'Bentely'];
+cars.push('Jeep', 'BMW', 'Ducati', 'Delorean', 'Chevy');
+console.log(cars);
+cars.pop();
+console.log(cars);
+cars.unshift('Jeep');
+console.log(cars);
+cars.shift();
+console.log(cars);
